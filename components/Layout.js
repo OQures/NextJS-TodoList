5bf597468a56f9591066e356f5abdad353799b2e
Ossama Qureshi <ossamasq1@outlook.com>
2023-01-29 14:45:58 -0500

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function Layout(props) {
+  const { children } = props;
+  return (
+    <div className="flex flex-col min-h-screen relative bg-slate-800">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
