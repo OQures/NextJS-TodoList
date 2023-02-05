f2515aa76ec69d42fc22235ac0e781fdfa6c6afb
Ossama Qureshi <ossamasq1@outlook.com>
2023-02-05 16:27:43 -0500

    Footer/ Signin Auth

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="flex justify-center items-center gap-3 py-3">
+      <a href="https://www.linkedin.com/in/ossamaqureshi/">
+        <i class="fa-brands fa-linkedin duration-300 hover:opacity-30 cursor-pointer"></i>
+      </a>
+      <a href="https://github.com/OQures">
+        <i class="fa-brands fa-github duration-300 hover:opacity-30 cursor-pointer"></i>
+      </a>
+      <a href="https://www.ossamaqureshi.com/">
+        <i class="fa-solid fa-palette duration-300 hover:opacity-30 cursor-pointer"></i>
+      </a>
+    </div>
+  );
 }
