1e267999ac2712745453934081859f80666a6944
Ossama Qureshi <ossamasq1@outlook.com>
2023-02-06 16:25:33 -0500

    Logout Menu P1

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Modal from "./Modal";
+import _document from "../pages/_document";
 
 export default function Header() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <div className="sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white">
-      <h1 className="text-2xl select-none sm:text-5xl duration-300 hover:opacity-100">
-        TODO / LIST
-      </h1>
-      <i className="fa-solid fa-user text-xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>
-    </div>
+    <>
+      {openModal && <Modal setOpenModal={setOpenModal} />}
+      <div className="sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white">
+        <h1 className="text-2xl select-none sm:text-5xl duration-300 hover:opacity-100">
+          TODO / LIST
+        </h1>
+        <i
+          onClick={() => setOpenModal(true)}
+          className="fa-solid fa-user text-xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"
+        ></i>
+      </div>
+    </>
   );
 }
