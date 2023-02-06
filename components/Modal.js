5cb3aa36beffca42dc2b2942933612a1e57723dd
Ossama Qureshi <ossamasq1@outlook.com>
2023-02-06 16:49:56 -0500

    logout functionality

--- components/Modal.js
+++ components/Modal.js
@@ -1,9 +1,11 @@
+import { useAuth } from "@/context/AuthContext";
 import React, { useState, useEffect } from "react";
 import ReactDom from "react-dom";
 
 export default function (props) {
   const { setOpenModal } = props;
   const [_document, set_document] = useState(null);
+  const { logout } = useAuth();
 
   useEffect(() => {
     set_document(document);
@@ -14,16 +16,26 @@ export default function (props) {
   }
 
   return ReactDom.createPortal(
-    <div className="fixed w-screen h-screen top-0 left-0 bg-white text-slate-900 flex flex-col">
+    <div className="fixed w-screen h-screen top-0 left-0 bg-white text-slate-900 flex flex-col text-lg sm:text-xl">
       <div className="flex items-center justify-between border-b border-solid border-slate-900 p-4">
-        <h1>Menu</h1>
+        <h1 className="font-bold text-2xl sm:text-4xl duration-300 select-none">
+          Menu
+        </h1>
         <i
           onClick={() => setOpenModal(false)}
-          className="fa-solid fa-xmark"
+          className="fa-solid fa-xmark duration-300 hover:rotate-90 cursor-pointer text-2xl"
         ></i>
       </div>
       <div className="p-4 flex flex-col gap-3">
-        <h2>Logout</h2>
+        <h2
+          onClick={() => {
+            logout();
+            setOpenModal(false);
+          }}
+          className="select-none duration-300 hover:pl-3 cursor-pointer"
+        >
+          Logout
+        </h2>
       </div>
     </div>,
     _document.getElementById("portal")
