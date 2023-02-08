9ed583986e2be78cd74fb313cb47322dbfb67f96
Ossama Qureshi <ossamasq1@outlook.com>
2023-02-07 20:31:18 -0500

    Add Todo Button/Input Field

--- components/Footer.js
+++ components/Footer.js
@@ -4,13 +4,13 @@ export default function Footer() {
   return (
     <div className="flex justify-center items-center gap-3 py-3">
       <a href="https://www.linkedin.com/in/ossamaqureshi/">
-        <i class="fa-brands fa-linkedin duration-300 hover:opacity-30 cursor-pointer"></i>
+        <i class="fa-brands fa-linkedin duration-300 hover:opacity-30 cursor-pointer text-2xl sm:text-4xl"></i>
       </a>
       <a href="https://github.com/OQures">
-        <i class="fa-brands fa-github duration-300 hover:opacity-30 cursor-pointer"></i>
+        <i class="fa-brands fa-github duration-300 hover:opacity-30 cursor-pointer text-2xl sm:text-4xl"></i>
       </a>
       <a href="https://www.ossamaqureshi.com/">
-        <i class="fa-solid fa-palette duration-300 hover:opacity-30 cursor-pointer"></i>
+        <i class="fa-solid fa-palette duration-300 hover:opacity-30 cursor-pointer text-2xl sm:text-4xl"></i>
       </a>
     </div>
   );
--- components/UserDashboard.js
+++ components/UserDashboard.js
@@ -1,5 +1,24 @@
-import React from "react";
+import { useAuth } from "@/context/AuthContext";
+import React, { useState } from "react";
 
 export default function UserDashboard() {
-  return <div>UserDashboard</div>;
+  const { userInfo } = useAuth();
+  const [addTodo, setAddTodo] = useState(false);
+  const [todo, setTodo] = useState("");
+
+  return (
+    <div className="w-full max-w-[65ch] mx-auto flex flex-col gap-3 sm: gap-5 text-xs sm:text-sm">
+      <input
+        type="text"
+        placeholder="Enter Todo"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        className="outline-none p-3 text-sm sm:text-lg text-slate-900 duration-300 rounded-xl"
+      />
+      {userInfo && <></>}
+      <button className="bg-cyan-300 text-slate-900 py-2 text-center uppercase text-lg rounded-xl duration-200 hover:opacity-30">
+        ADD TODO
+      </button>
+    </div>
+  );
 }
