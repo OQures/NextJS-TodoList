0f78c5179f3d149f13b5061c6c960ed43d8f4bb2
Ossama Qureshi <ossamasq1@outlook.com>
2023-02-07 20:48:49 -0500

    Set Add Todo Logic

--- components/UserDashboard.js
+++ components/UserDashboard.js
@@ -8,17 +8,29 @@ export default function UserDashboard() {
 
   return (
     <div className="w-full max-w-[65ch] mx-auto flex flex-col gap-3 sm: gap-5 text-xs sm:text-sm">
-      <input
-        type="text"
-        placeholder="Enter Todo"
-        value={todo}
-        onChange={(e) => setTodo(e.target.value)}
-        className="outline-none p-3 text-sm sm:text-lg text-slate-900 duration-300 rounded-xl"
-      />
+      {addTodo && (
+        <div className="flex items-stretch">
+          <input
+            type="text"
+            placeholder="Enter Todo"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            className="outline-none p-3 text-sm sm:text-lg text-slate-900 duration-300 flex-1 rounded-l-xl"
+          />
+          <button className="w-fit px-4 sm:px-6 py-2 sm:py-3 bg-amber-400 text-white font-medium text-base rounded-r-xl duration-300 hover:opacity-40">
+            ADD
+          </button>
+        </div>
+      )}
       {userInfo && <></>}
-      <button className="bg-cyan-300 text-slate-900 py-2 text-center uppercase text-lg rounded-xl duration-200 hover:opacity-30">
-        ADD TODO
-      </button>
+      {!addTodo && (
+        <button
+          onClick={() => setAddTodo(true)}
+          className="bg-cyan-300 text-slate-900 py-2 text-center uppercase text-lg rounded-xl duration-200 hover:opacity-30"
+        >
+          ADD TODO
+        </button>
+      )}
     </div>
   );
 }
